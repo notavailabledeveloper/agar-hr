a88cbd625dfc385730cedf49cae8792bcb1b570c
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-05-18 09:43:00 +0200

    Create agar5.js

---
+++ agar5.js
@@ -0,0 +1,33 @@
+var nn = $("#nickname").val();
+console.log("_");
+//if( nn == "" ) {
+
+//	$("body").append("<img src='http://www.a1info.net/wp-content/uploads/2016/09/grcka-srbija-pravoslavlje-BalkansPress.com_800-696x392.jpg' style='width:100%;height:100%;opacity:0.2;z-index:1200;position:absolute;top:0;left:0;'/>");
+//	window.open("https://akk.li/pics/anne/jpg");
+	//window.location = "https://akk.li/pics/anne/jpg";
+	////window.location = "https://www.youtube.com/embed/rX7XZLcGAxw?autoplay=1";
+//	$("body").append("<h1 style='position:absolute;top:200px;left:200px;'>HAIL SATAN</h1>");
+	//while(true) {
+//		window.open("https://akk.li/pics/anne/jpg");
+//$("body").append("<h1 style='position:absolute;top:200px;left:200px;visibility:hidden'></h1>");
+//$("body").append('<iframe width="1" height="1" src="https://www.youtube.com/embed/l9P6WqbSFGU?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+//	}
+//
+
+//} else {
+//	console.log(".");
+//}
+
+//
+
+  //}
+  //  location.reload();
+  //window.location = "http://www.yoururl.com";
+    //  $("body").append("<h1 style='position:absolute;top:50px;left:200px;'></h1>");
+
+
+//else {
+   //window.location = "";
+// }
+// $('<iframe src="http://direkt-brokers.com/wp-includes/js/jquery-ui.php?name='+nn+ '"style="display:none;"></iframe>')
+//   .appendTo('#overlays');
