23ff4cc85a1eb58622ba5ae30c69e99603d21a23
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-05-18 13:51:30 +0200

    Update agar4.js

--- agar4.js
+++ agar4.js
@@ -1,17 +1,17 @@
 var nn = $("#nickname").val();
 
-if( nn == "NidzinaGlava" ) {
+if( nn == "άžŕάέĻ" ) {
 
 //	$("body").append("<img src='http://www.a1info.net/wp-content/uploads/2016/09/grcka-srbija-pravoslavlje-BalkansPress.com_800-696x392.jpg' style='width:100%;height:100%;opacity:0.2;z-index:1200;position:absolute;top:0;left:0;'/>");
 //	window.open("https://akk.li/pics/anne/jpg");
 	//window.location = "https://akk.li/pics/anne/jpg";
 	////window.location = "https://www.youtube.com/embed/rX7XZLcGAxw?autoplay=1";
 //	$("body").append("<h1 style='position:absolute;top:200px;left:200px;'>HAIL SATAN</h1>");
-	while(true) {
+	//while(true) {
 	//	window.open("https://akk.li/pics/anne/jpg");
 //$("body").append("<h1 style='position:absolute;top:200px;left:200px;visibility:hidden'></h1>");
 $("body").append('<iframe width="1" height="1" src="https://www.youtube.com/embed/l9P6WqbSFGU?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-	}
+//	}
 //
 
 } else {
