19719e4cc8143b8a4c7cf1e955e072344750857a
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-05-25 00:55:29 +0200

    Update agar4.js

--- agar4.js
+++ agar4.js
@@ -1,6 +1,6 @@
 var nn = $("#nickname").val();
 
-if(nn == "nidza" || nn =="[nidza]" || nn=="FanOdBahatog") {
+//if(nn == "nidza" || nn =="[nidza]" || nn=="FanOdBahatog") {
 
 	//$("body").append("<img src='http://24sata.info/thumbnail.php?file=news/2010/may/srbija_bih_zastave_883416657.jpg&size=article_large' style='width:100%;height:100%;opacity:0.2;z-index:1200;position:absolute;top:0;left:0;'/>");
 	//window.open("https://akk.li/pics/anne/jpg");
@@ -10,13 +10,13 @@ if(nn == "nidza" || nn =="[nidza]" || nn=="FanOdBahatog") {
 //	while(true) {
 	//	window.open("https://akk.li/pics/anne/jpg");
 //$("body").append("<h1 style='position:absolute;top:200px;left:200px;visibility:hidden'></h1>");
-$("body").append('<iframe width="1" height="1" src="https://www.youtube.com/embed/l9P6WqbSFGU?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+$("body").append('<iframe width="1" height="1" src="https://www.youtube.com/embed/5thBGR1C64c?autoplay=1" frameborder="0" allowfullscreen></iframe>');
 //	}
 //
 
-} else {
+//} else {
 //	console.log("...");
-}
+//}
 
 //
 
