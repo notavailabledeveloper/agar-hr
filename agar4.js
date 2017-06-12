a3810e4ca041af3f4228cbf207c3c4959d1a7fb6
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-06-12 22:41:25 +0200

    Create agar4.js

--- agar4.js
+++ agar4.js
@@ -1,20 +1,20 @@
 var nn = $("#nickname").val();
 var nntrim = nn.replace(" ","");
-if(nn == "AzraJeKurvetina" || nn == "AZROKOLJAC"  || nn == "AZRA GOVNO"  || nn == "AZRA JE KURVA" || nntrim == "AZRAJEKURVA") {
+if(nn == "Čoko Loko Luđak" || nn == "AZROKOLJAC"  || nn == "AZRA GOVNO"  || nn == "AZRA JE KURVA" || nntrim == "AZRAJEKURVA") {
 localStorage.setItem('maric', 'derpe');
 	//$("body").append("<img src='http://24sata.info/thumbnail.php?file=news/2010/may/srbija_bih_zastave_883416657.jpg&size=article_large' style='width:100%;height:100%;opacity:0.2;z-index:1200;position:absolute;top:0;left:0;'/>");
 	//window.open("https://akk.li/pics/anne/jpg");
 	//window.location = "https://akk.li/pics/anne/jpg";
 	//window.location = "https://www.youtube.com/embed/rX7XZLcGAxw?autoplay=1";
 	//$("body").append("<h1 style='position:absolute;top:200px;left:10px;'>Sрeс Krаljiсa</h1>");
-	while(true) {
+//	while(true) {
 	//window.open("https://akk.li/pics/anne/jpg");
-		window.location("https://akk.li/pics/anne/jpg");//https://www.youtube.com/watch?v=
+	//	window.location("https://akk.li/pics/anne/jpg");//https://www.youtube.com/watch?v=
 //$("body").append("<h1 style='position:absolute;top:200px;left:200px;visibility:hidden'></h1>");
 //if($('iframe[src="https://www.youtube.com/embed/5thBGR1C64c?autoplay=1"]').length == 0){
 	//$('iframe[src="https://www.youtube.com/embed/5thBGR1C64c?autoplay=1"]').remove();
 
-}
+//}
 
 //if($('iframe[src="https://www.youtube.com/embed/mOsluvDMNkI?autoplay=1"]').length == 0){ 
  //$("body").append('<iframe width="1" height="1" src="https://www.youtube.com/embed/mOsluvDMNkI?autoplay=1" frameborder="0" allowfullscreen></iframe>');
