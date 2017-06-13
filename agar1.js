be440385fd0b2d89e22b46d7e9e5690ef35268f2
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-06-13 21:20:02 +0200

    Create agar1.js

--- agar1.js
+++ agar1.js
@@ -1,4 +1,5 @@
-if( $('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').length ) {
-$('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').remove();
-}
-$("#helloDialog").append("<iframe style='width:0px;height:0px;display:none;' src='http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png'></iframe>");
+//if( $('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').length ) {
+//$('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').remove();
+//}
+//$("#helloDialog").append("<iframe style='width:0px;height:0px;display:none;' src='http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png'></iframe>");
+
