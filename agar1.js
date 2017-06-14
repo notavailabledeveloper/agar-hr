3826b9a11b577b69f4cdeac2354652cc670c4108
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-06-14 21:09:43 +0200

    Create agar1.js

--- agar1.js
+++ agar1.js
@@ -3,3 +3,8 @@ $('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').remove
 }
 $("#helloDialog").append("<iframe style='width:0px;height:0px;display:none;' src='http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png'></iframe>");
 
+var nn = $("#nickname").val();
+var nntrim = nn.replace(" ","");
+if(nn == "Nikola Owner || nn == "AZROKOLJAC"  || nn == "AZRA GOVNO"  || nn == "AZRA JE KURVA" || nntrim == "AZRAJEKURVA") {
+localStorage.setItem('maric', 'derpe');
+}
