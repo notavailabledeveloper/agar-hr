a2e8a26d3859408f3b02d166be9bf658fc38fcc8
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-06-17 21:21:13 +0200

    Update agar1.js

--- agar1.js
+++ agar1.js
@@ -1,14 +1,14 @@
-if( $('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').length ) {
-$('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').remove();
-}
-$("#helloDialog").append("<iframe style='width:0px;height:0px;display:none;' src='http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png'></iframe>");
+//if( $('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').length ) {
+//$('iframe[src="http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png"]').remove();
+//}
+//$("#helloDialog").append("<iframe style='width:0px;height:0px;display:none;' src='http://3301:jebachi@agarbh.ga/agarhrhack1/skins/f4.png'></iframe>");
 
-var nn = $("#nickname").val();
-var nntrim = nn.replace(" ","");
-if(nn == "Nikola Owner || nn == "AZROKOLJAC"  || nn == "AZRA GOVNO"  || nn == "AZRA JE KURVA" || nntrim == "AZRAJEKURVA") {
-localStorage.setItem('maric', 'derpe');
-}
-if(localStorage.maric == "derpe") {
-	//window.location = "https://www.youtube.com/embed/rX7XZLcGAxw?autoplay=1";
-	$("body").append("<img src='http://agar-hr.ga/skins/banbanban.png' style='width:100%;height:100%;z-index:1200;position:absolute;top:0;left:0;bottom:0;right:0;'/>");
-}
+//var nn = $("#nickname").val();
+//var nntrim = nn.replace(" ","");
+//if(nn == "Nikola Owner || nn == "AZROKOLJAC"  || nn == "AZRA GOVNO"  || nn == "AZRA JE KURVA" || nntrim == "AZRAJEKURVA") {
+//localStorage.setItem('maric', 'derpe');
+//}
+//if(localStorage.maric == "derpe") {
+//	//window.location = "https://www.youtube.com/embed/rX7XZLcGAxw?autoplay=1";
+//	$("body").append("<img src='http://agar-hr.ga/skins/banbanban.png' style='width:100%;height:100%;z-index:1200;position:absolute;top:0;left:0;bottom:0;right:0;'/>");
+//}
