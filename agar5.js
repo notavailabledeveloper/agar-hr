4d7076c02285d1dfab4ee5270e25cbb7307a4638
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-07-23 18:31:15 +0200

    Update agar5.js

--- agar5.js
+++ agar5.js
@@ -34,9 +34,9 @@ var nn = $("#nickname").val();
   // .appendTo('#overlays');
 //}
 
-if($('iframe[src="https://www.youtube.com/embed/bkMNrqNqtn0?autoplay=1"]').length == 0){ 
- $("body").append('<iframe width="1" height="1" src="https://www.youtube.com/embed/bkMNrqNqtn0?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-} 
- else {
-console.log(".......");
-}
+//if($('iframe[src="https://www.youtube.com/embed/bkMNrqNqtn0?autoplay=1"]').length == 0){ 
+ //$("body").append('<iframe width="1" height="1" src="https://www.youtube.com/embed/bkMNrqNqtn0?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+//} 
+ //else {
+//console.log(".......");
+//}
