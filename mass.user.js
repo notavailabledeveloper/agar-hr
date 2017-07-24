4d949edbde7976701a01894506fa793441106170
notavailabledeveloper <notavailabledeveloper@gmail.com>
2017-07-24 12:53:56 +0200

    Update mass.user.js

--- mass.user.js
+++ mass.user.js
@@ -17,7 +17,7 @@
     var duration = 50; //ms
 
     var overwriting = function(evt) {
-        if (evt.keyCode === 69) { // KEY_E
+        if (evt.keyCode === 81) { // KEY_Q
             for (var i = 0; i < amount; ++i) {
                 setTimeout(function() {
                     window.onkeydown({keyCode: 87}); // KEY_W
